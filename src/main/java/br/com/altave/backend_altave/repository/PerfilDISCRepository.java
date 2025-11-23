6e2f4d14e28ddb99260ab9765be556a32d0976b1
cleberkirch86 <cleberkirch@gmail.com>
2025-11-23 19:34:51 -0300

    Corrigindo bug no VistaColaboradores

--- src/main/java/br/com/altave/backend_altave/repository/PerfilDISCRepository.java
+++ src/main/java/br/com/altave/backend_altave/repository/PerfilDISCRepository.java
@@ -4,6 +4,7 @@ import br.com.altave.backend_altave.model.PerfilDISC;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
